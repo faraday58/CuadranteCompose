f61ac7bfc715bc0fc765176991adbd8c50ec8ed3
Armando <jarodriguezvera@gmail.com>
2023-12-10 17:28:37 -0600

    Change Color to correct

--- app/src/main/java/com/mexiti/cuadrantecompose/MainActivity.kt
+++ app/src/main/java/com/mexiti/cuadrantecompose/MainActivity.kt
@@ -5,23 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -41,6 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    DivideCompose()
 
                 }
             }
@@ -99,7 +94,7 @@ fun DivideCompose( modifier: Modifier = Modifier) {
                 Modifier
                     .weight(weight = 0.5F)
                     .fillMaxHeight()
-                    .background(Color(0xFFD0BCF))
+                    .background(Color(0xFFD0BCFF))
                 ,
                 verticalArrangement = Arrangement.Center
 
